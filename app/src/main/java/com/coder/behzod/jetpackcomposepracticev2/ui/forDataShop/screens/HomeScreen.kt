1b5ef6d2c93cf49031c7842f6f143c324758217d
Behzod <behzod96447@internet.ru>
2024-03-12 17:56:14 +0300

    working on home screen and optimizing codes

--- app/src/main/java/com/coder/behzod/jetpackcomposepracticev2/ui/forDataShop/screens/HomeScreen.kt
+++ app/src/main/java/com/coder/behzod/jetpackcomposepracticev2/ui/forDataShop/screens/HomeScreen.kt
@@ -1,23 +1,19 @@
 package com.coder.behzod.jetpackcomposepracticev2.ui.forDataShop.screens
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyItemScope
-import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalMapOf
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.coder.behzod.jetpackcomposepracticev2.R
@@ -31,10 +27,9 @@ import com.coder.behzod.jetpackcomposepracticev2.ui.forDataShop.isCompleted.SetT
 import com.coder.behzod.jetpackcomposepracticev2.ui.views.AdsHorizontalPager
 import com.coder.behzod.jetpackcomposepracticev2.ui.views.CategoryRow
 import com.coder.behzod.jetpackcomposepracticev2.ui.views.CategoryView
-import kotlin.math.PI
 
 @Composable
-fun HomeScreen(navController:NavHostController) {
+fun HomeScreen(navController: NavHostController) {
     val productList = ArrayList<ProductModel>()
     productList.add(PRODUCT_MODEL)
     productList.add(PRODUCT_MODEL)
@@ -48,22 +43,18 @@ fun HomeScreen(navController:NavHostController) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-    ) {
+    ){
         SetTopAppBar(painter = painterResource(id = R.drawable.ic_call))
-        Spacer(modifier = Modifier.height(5.dp))
-        SearchBar(navController)
-        Spacer(modifier = Modifier.height(10.dp))
-        LazyColumn (
+        LazyVerticalGrid(
             modifier = Modifier
-                .fillMaxSize()
-        ){
-            item{
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(Color(0xFFFFFFFF))
-                        .padding(10.dp)
-                ) {
+                .fillMaxSize(),
+            columns = GridCells.Fixed(2)
+        ) {
+            item (span = { GridItemSpan(2) }){
+                Column {
+                    Spacer(modifier = Modifier.height(5.dp))
+                    SearchBar(navController)
+                    Spacer(modifier = Modifier.height(10.dp))
                     AdsHorizontalPager(adsModel = ADS_MODEL)
                     Spacer(modifier = Modifier.height(10.dp))
                     CategoryView()
@@ -72,13 +63,16 @@ fun HomeScreen(navController:NavHostController) {
                     Spacer(modifier = Modifier.height(10.dp))
                 }
             }
-            items(productList){
-                LazyVerticalGrid(columns = GridCells.Fixed(2)) {
-                    items(productList){
-                        ProductsItem(product = it)
-                    }
-                }
+            items(productList,span = { GridItemSpan(1) }) {
+                ProductsItem(product = it)
             }
         }
     }
+}
+
+@Preview
+@Composable
+private fun PreviewHS() {
+    val context = LocalContext.current
+    HomeScreen(navController = NavHostController(context))
 }
